543f29d66d0aea547c0cfdba336812030c67e18e
stemasoff <stemasoff1@yandex.ru>
2019-11-11 08:17:42 +0300

    Added tests.

--- src/main/java/WallpaperCalculatorService.java
+++ src/main/java/WallpaperCalculatorService.java
@@ -7,17 +7,17 @@ public class WallpaperCalculatorService {
 
     public int calculate(WallpaperCalculator parametersForCalculation) {
         perimeter = (parametersForCalculation.getLengthRoom() + parametersForCalculation.getWidthRoom()) * 2;
-        amountWallpaperPanels = (perimeter / parametersForCalculation.getWidthWallpaper()) + 1;
+        amountWallpaperPanels = perimeter / parametersForCalculation.getWidthWallpaper() + 1;
 
         if (parametersForCalculation.isRaport() == false) {
-            amountStrip = parametersForCalculation.getLengthWallpaperRoll() /
-                    (parametersForCalculation.getHeightRoom() + parametersForCalculation.getAdditionalStock());
+            amountStrip = parametersForCalculation.getLengthWallpaperRoll()
+                    / (parametersForCalculation.getHeightRoom() + parametersForCalculation.getAdditionalStock());
 
         }
         else {
-            lengthOneStrip = (parametersForCalculation.getHeightRoom() / parametersForCalculation.getRaportValue()) + 1;
-            amountStrip = parametersForCalculation.getLengthWallpaperRoll() /
-                    (lengthOneStrip * parametersForCalculation.getRaportValue());
+            lengthOneStrip = parametersForCalculation.getHeightRoom() / parametersForCalculation.getRaportValue() + 1;
+            amountStrip = parametersForCalculation.getLengthWallpaperRoll()
+                    / (lengthOneStrip * parametersForCalculation.getRaportValue());
         }
 
         calculateRolls = amountWallpaperPanels / amountStrip;
--- src/test/java/WallpaperCalculatorServiceTest.java
+++ src/test/java/WallpaperCalculatorServiceTest.java
@@ -5,7 +5,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class WallpaperCalculatorServiceTest {
 
     @Test
-    void calculate() {
+    void calculateRaport() {
+        WallpaperCalculator parametersForCalculation = new WallpaperCalculator();
+        WallpaperCalculatorService calculator = new WallpaperCalculatorService();
+
+        parametersForCalculation.setLengthRoom(500);
+        parametersForCalculation.setWidthRoom(600);
+        parametersForCalculation.setWidthWallpaper(106);
+        parametersForCalculation.setLengthWallpaperRoll(1_000);
+        parametersForCalculation.setHeightRoom(275);
+        parametersForCalculation.setRaport(true);
+        parametersForCalculation.setRaportValue(30);
+
+        assertEquals(7, calculator.calculate(parametersForCalculation));
+
+    }
+
+    @Test
+    void calculateNoRaport() {
         WallpaperCalculator parametersForCalculation = new WallpaperCalculator();
         WallpaperCalculatorService calculator = new WallpaperCalculatorService();
 
@@ -15,8 +32,7 @@ class WallpaperCalculatorServiceTest {
         parametersForCalculation.setLengthWallpaperRoll(1_000);
         parametersForCalculation.setHeightRoom(275);
         parametersForCalculation.setAdditionalStock(10);
-        parametersForCalculation.setRaport(true);
-        parametersForCalculation.setRaportValue(30);
+        parametersForCalculation.setRaport(false);
 
         assertEquals(7, calculator.calculate(parametersForCalculation));
 
